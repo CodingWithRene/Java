12c13cca375cf260d8153bef546c41ef065e0434
CodingWithRene <vanderwattrene@gmail.com>
2019-07-10 11:53:56 -0600

    Intro to java source
    I added the first project in the java basics playlist

---
+++ IntroToJava/src/App.java
@@ -0,0 +1,32 @@
+
+public class App {
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello World");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
