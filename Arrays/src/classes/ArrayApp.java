c6bc8e5e5c861fa7f836e69558361fd4490d6aec
CodingWithRene <vanderwattrene@gmail.com>
2019-07-23 22:38:21 -0600

    Added Array

---
+++ Arrays/src/classes/ArrayApp.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+
+		int[] haha = { 1, 2, 3, 4 };
+		// 1D array
+		int[] numbers = new int[10];
+		numbers[0] = 1123;
+		numbers[4] = 36;
+
+		// 2D array
+
+		int[][] twoDArray = { { 10, 36 }, { 5, 42 } };
+		int[][] another = new int[5][2];
+		
+		System.out.println(twoDArray[1][1]);
+
+	}
+
+}
